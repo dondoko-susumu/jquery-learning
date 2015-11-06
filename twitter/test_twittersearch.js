9c4c802e8ec2fa0e13e7f22ba4b022289464872a
Ken Kubota <kkbt2003@gmail.com>
2015-11-06 14:30:23 +0900

    add test case keyword.

--- twitter/test_twittersearch.js
+++ twitter/test_twittersearch.js
@@ -61,4 +61,56 @@ jQuery(function($){
     equal(params.lang.val(), 'ja','言語(lang)の初期値はja');
 
   });
+
+  module('つぶやきのテキストを表示用に編集',{
+    setup: function() {
+      if (!TwitterSearch.view) TwitterSearch.init();
+    }
+  });
+
+  test('リンクの追加', function(){
+    var renderText = TwitterSearch.renderText;
+    equal(renderText('jQuery', '@gihyo'),
+      '<a class="search" href="javascript:void(0);">@gihyo</a>',
+      '@ユーザー名(replyまたはmention)にリンク追加');
+    equal(renderText('jQuery', '#gihyo'),
+      '<a class="search" href="javascript:void(0);">#gihyo</a>',
+      'ハッシュタグにリンク追加');
+    equal(renderText('jQuery', 'http://gihyo.jp/'),
+      '<a target="_blank" href="http://gihyo.jp/">http://gihyo.jp/</a>',
+      '外部サイトへのリンク追加');
+  });
+
+  test('キーワードの強調', function(){
+    var renderText = TwitterSearch.renderText;
+    var keyword = '"jQuery UI" Or jQuery';
+
+    //キーワードの強調のみ
+    equal(renderText(keyword ,'jQuery jQuery UI'),
+      '<span class="query">jQuery</span> ' +
+      '<span class="query">jQuery UI</span>' ,
+      'キーワードの強調("〜"への対応)');
+    equal(renderText(keyword ,'jquery jquery ui'),
+      '<span class="query">jquery</span> ' +
+      '<span class="query">jquery ui</span>' ,
+      'キーワードの強調(小文字)');
+    equal(renderText(keyword ,'JQUERY JQUERY ui'),
+      '<span class="query">JQUERY</span> ' +
+      '<span class="query">JQUERY ui</span>' ,
+      'キーワードの強調(大文字・小文字混在)');
+
+    // リンクとの組み合わせ（キーワードがハッシュタグなどにも含まれる場合）
+    equal(renderText(keyword ,'jQuery UI @jQuery'),
+      '<span class="query">jQuery UI</span> ' +
+      '<a class="search" href="javascript:void(0);">@<span class="query">jQuery</span></a>',
+      'リンク内でもキーワード強調(@ユーザー名)');
+    equal(renderText(keyword ,'jQuery UI #jQuery'),
+      '<span class="query">jQuery UI</span> ' +
+      '<a class="search" href="javascript:void(0);">#<span class="query">jQuery</span></a>',
+      'リンク内でもキーワード強調(#ハッシュタグ)');
+    equal(renderText(keyword ,'http://jqueryui.com/ http://jquery.com/'),
+      '<a target="_blank" href="http://jqueryui.com/">http://<span class="query">jquery</span>ui.com/</a> ' +
+      '<a target="_blank" href="http://jquery.com/">http://<span class="query">jquery</span>.com/</a>' ,
+      'リンク内でもキーワード強調(外部サイト)');
+  });
 });
